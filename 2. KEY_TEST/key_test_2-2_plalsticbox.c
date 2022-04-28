e894cf0f6425a793280fe74c8beb845690b4ee83
jysong999 <75262539+jysong999@users.noreply.github.com>
2022-04-28 15:31:45 +0900

    Update key_test_2-2_plalsticbox.c

--- 2. KEY_TEST/key_test_2-2_plalsticbox.c
+++ 2. KEY_TEST/key_test_2-2_plalsticbox.c
@@ -1,9 +1,7 @@
 
-//ÀüÀÚ°øÇÐºÎ 201918147 ¼ÛÀç¿µ
+//Futher Example 2-2    : í”Œë¼ìŠ¤í‹±ë°•ìŠ¤ ìž¥ë¹„ ì‚¬ìš©
 
-//Futher Example 2-2    : ÇÃ¶ó½ºÆ½¹Ú½º Àåºñ »ç¿ë
-
-//Key¸¦ ´©¸£¸é ¿¬°áµÈ LED°¡ ¾Æ·¡ Ç¥¿¡ ¾à¼ÓµÈ ¹æ¹ýÀ¸·Î 2°³¾¿ ÄÑÁø´Ù
+//Keyë¥¼ ëˆ„ë¥´ë©´ ì—°ê²°ëœ LEDê°€ ì•„ëž˜ í‘œì— ì•½ì†ëœ ë°©ë²•ìœ¼ë¡œ 2ê°œì”© ì¼œì§„ë‹¤
 //Key 4    LED 5,6 
 //Key 5    LED 6,7 
 //Key 6    LED 7,4 
@@ -19,16 +17,16 @@ BYTE S_PORTD, S_PORTE;
 
 void main(void)
 {
-    //PORTÃÊ±âÈ­
-    DDRD = 0x00;            //Key : PORTD : ÀÔ·Â = 0
-    PORTD = 0xf0;           //»óÀ§ 4ºñÆ®¸¸ »ç¿ë
-    DDRE = 0xff;            //LED : PORTE : Ãâ·Â = 1
-    PORTE = 0x00;           //KEY¸¦ ´©¸£Áö ¾Ê¾ÒÀ» ¶§ ¸ðµÎ ²¨Áø »óÅÂ
+    //PORTì´ˆê¸°í™”
+    DDRD = 0x00;            //Key : PORTD : ìž…ë ¥ = 0
+    PORTD = 0xf0;           //ìƒìœ„ 4ë¹„íŠ¸ë§Œ ì‚¬ìš©
+    DDRE = 0xff;            //LED : PORTE : ì¶œë ¥ = 1
+    PORTE = 0x00;           //KEYë¥¼ ëˆ„ë¥´ì§€ ì•Šì•˜ì„ ë•Œ ëª¨ë‘ êº¼ì§„ ìƒíƒœ
     
     while(1){
     
-        S_PORTD = (PIND&0xf0);   // ÀÓ½Ã ¹öÆÛ »ý¼º, »óÀ§ 4ºñÆ®¸¦ ÀÐ¾î¼­ ÀúÀå
-        S_PORTE = (PORTE&0xf0);  // ÀÓ½Ã ¹öÆÛ »ý¼º, µ¿ÀÛ¿¡ »ó°ü ¾ø´Â ÇÏÀ§ 4ºñÆ®´Â MASKÃ³¸® 
+        S_PORTD = (PIND&0xf0);   // ìž„ì‹œ ë²„í¼ ìƒì„±, ìƒìœ„ 4ë¹„íŠ¸ë¥¼ ì½ì–´ì„œ ì €ìž¥
+        S_PORTE = (PORTE&0xf0);  // ìž„ì‹œ ë²„í¼ ìƒì„±, ë™ìž‘ì— ìƒê´€ ì—†ëŠ” í•˜ìœ„ 4ë¹„íŠ¸ëŠ” MASKì²˜ë¦¬ 
         
         if(S_PORTD != S_PORTE){
             
